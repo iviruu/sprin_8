c1f16592cd1f5abb464d6f4aeb528d3f307a1064
iviruu <ivan.kok.ru@gmail.com>
2024-05-12 21:07:35 +0200

    modificacion del nombre

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     endpoint: 'http://localhost:3000/',
-    apiKey: 'pk.eyJ1IjoibGF1cmFkbCIsImEiOiJjbHZ0bzh0aTYxMTdvMmpycnB3aHc5aGw4In0.dUks0DJBsTp5CkbXy9cKqA'
+    apiKey: ''
 };
